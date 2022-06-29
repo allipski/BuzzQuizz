03dfb146e5d1c1312046d0dda676b495b9a66cff
AleCostaS <alexandrec.silva@usp.br>
2022-06-29 17:07:11 -0300

    validando a cor

--- javascript/criandoperguntas.js
+++ javascript/criandoperguntas.js
@@ -10,7 +10,7 @@ function criarPerguntas(){
                         <ion-icon name="open-outline" onClick="abrirPergunta(this)"></ion-icon>
                         <h2>Pergunta ${i}</h2>
                         <input type="text" placeholder="Texto da pergunta">
-                        <input type="text" placeholder="Cor de fundo da pergunta">
+                        <input type="text" class="cor" placeholder="Cor de fundo da pergunta">
                         <h2>Resposta correta</h2>
                         <input type="text" placeholder="Resposta correta">
                         <input type="text" placeholder="URL da imagem">
@@ -30,6 +30,8 @@ function criarPerguntas(){
     const pergunta = document.querySelector(".secondform")
     pergunta.classList.remove("minimizado")
     pergunta.classList.add("visivel")
+
+    pagina.innerHTML += `<button onClick="proseguirCriarNiveis()">Prosseguir para criar níveis</button>`
 }
 
 criarPerguntas();
@@ -40,4 +42,40 @@ function abrirPergunta(elemento){
     perguntaAnterior.classList.remove("visivel")
     perguntaAnterior.classList.add("minimizado")
     elemento.parentNode.parentNode.classList.add("visivel")
+}
+
+function validarCor(){
+    let valida = true
+    let cor = document.querySelector(".visivel .cor")
+    if (cor.value.startsWith("#") && cor.value.length == 7){
+        let array = cor.value.split('')
+        for (let i = 1;i < 7;i++){
+            if ((array[i] == "A") || (array[i] == "a") || (array[i] == "B") || (array[i] == "b") || (array[i] == "C") || (array[i] == "c") && (array[i] == "D") || (array[i] == "d") || (array[i] == "E") || (array[i] == "e") || (array[i] == "F") || (array[i] == "f") || (array[i] == "1") || (array[i] == "2") || (array[i] == "3") || (array[i] == "4") || (array[i] == "5") || (array[i] == "6") || (array[i] == "7") || (array[i] == "8") || (array[i] == "9")){
+                console.log("aqui")
+            } else {
+                valida = false
+                i = 8
+            }
+        }
+    }  else {
+        valida = false
+    }
+
+    var erroCor = document.querySelector(".erroCor");
+    if(erroCor) {
+        erroCor.parentNode.removeChild(erroCor);
+    }
+
+    if (valida == false){
+        cor.classList.add("invalida")
+        cor.insertAdjacentHTML('afterend', '<p class="erroCor">A cor deve estar na forma hexadecimal</p>')
+    } else {
+        cor.classList.remove("invalida")
+    }
+
+    console.log(valida)
+}
+
+function proseguirCriarNiveis() {
+    validarCor();   
 }
