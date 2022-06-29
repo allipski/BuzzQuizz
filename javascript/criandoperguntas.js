d8332c9752ea3aaccad41d4cd447094f2a2de5bf
AleCostaS <alexandrec.silva@usp.br>
2022-06-29 19:00:23 -0300

    Bonus: Mostrando as entradas invalidas pro usuario

--- javascript/criandoperguntas.js
+++ javascript/criandoperguntas.js
@@ -61,41 +61,58 @@ function validarCor(){
         valida = false
     }
 
-    var erroCor = document.querySelector(".erroCor");
-    if(erroCor) {
-        erroCor.parentNode.removeChild(erroCor);
-    }
-
-    if (valida == false){
-        cor.classList.add("invalida")
-        cor.insertAdjacentHTML('afterend', '<p class="erroCor">A cor deve estar na forma hexadecimal</p>')
-    } else {
-        cor.classList.remove("invalida")
-    }
+    invalida(cor, valida, "A cor deve estar na forma hexadecimal", "erroCor");
 }
 
 function validarPergunta () {
-    const formulacao = document.querySelector(".pergunta")
+    const formulacao = document.querySelector(".visivel .pergunta")
     let valida = true
     
     if (formulacao.value.length < 20) {
         valida = false
     } 
 
-    var erroPergunta = document.querySelector(".erroPergunta");
-    if(erroPergunta) {
-        erroPergunta.parentNode.removeChild(erroPergunta);
+    invalida(formulacao, valida, "A pergunta deve ter no mínimo 20 caracteres", "erroPergunta")
+}
+
+function invalida(input, valida, mensagemErro, nomeErro){
+    var erro = document.querySelector("."+nomeErro);
+    if(erro) {
+        erro.parentNode.removeChild(erro);
     }
 
     if (valida == false){
-        formulacao.classList.add("invalida")
-        formulacao.insertAdjacentHTML('afterend', '<p class="erroPergunta">A pergunta deve ter no mínimo 20 caracteres</p>')
+        console.log("aqui")
+        input.classList.add("invalida")
+        input.insertAdjacentHTML('afterend', '<p class='+nomeErro+'>'+mensagemErro+'</p>')
     } else {
-        formulacao.classList.remove("invalida")
+        input.classList.remove("invalida")
+    }
+}
+
+function validarRespostas () {
+    const resposta = document.querySelector(".resposta")
+    let valida = true
+    
+    if (resposta) {
+        valida = false
+    } 
+
+    var erroResposta = document.querySelector(".erroResposta");
+    if(erroResposta) {
+        erroResposta.parentNode.removeChild(erroResposta);
+    }
+
+    if (valida == false){
+        resposta.classList.add("invalida")
+        resposta.insertAdjacentHTML('afterend', '<p class="erroResposta">A pergunta deve ter no mínimo 20 caracteres</p>')
+    } else {
+        resposta.classList.remove("invalida")
     }
 }
 
 function proseguirCriarNiveis() {
-    validarCor();
-    validarPergunta();   
+    validarCor()
+    validarPergunta()   
+    validarRespostas()
 }
