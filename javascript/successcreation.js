a38c8aa796796c204da0cbd5ce1a7b268898988a
allipski <alicematobalipski@gmail.com>
2022-07-01 16:47:44 -0300

    finalizacao pag de sucesso da criacao

--- javascript/successcreation.js
+++ javascript/successcreation.js
@@ -1,18 +1,21 @@
+let quizzImg;
+
 function loadpage2 () {
     // codigo para pegar img do servidor vai aqui
-    // const quizatual = axios.get ('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/ID_DO_QUIZZ');
-    // quizatual.then(getCoverFromQuizz(quizatual));
-    // quizatual.catch();
+    const quizatual = axios.get ('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/'+idQuizz);
+    quizatual.catch(mostrarErro).then(function(quizatual){
+        quizzImg = data.data.image;
+    });
 
     // codigo pra carregar o html da pagina
     pagina.innerHTML = 
     `<h1>Seu quizz está pronto!</h1>
     <div class="formatquizzsuccess">
-        <div class="degrade" onclick="loadQuizz()" >
-            <img src="https://daora.app/media/resize/1920x1080/pasta/2/6255dc61eb84f.jpg" />
+        <div class="degrade" onclick="exibirQuizz(${idQuizz})" >
+            <img src="${quizzImg}" />
         </div>
         <p class="titulonaimagem">Titulo do quiz</p>
     </div>
-    <button type="submit" onclick="loadQuizz()">Acessar Quizz</button>
+    <button type="submit" onclick="exibirQuizz(${idQuizz})">Acessar Quizz</button>
     <p class="gohome" onclick="criarPagina()">Voltar pra home</p>`;
 }
