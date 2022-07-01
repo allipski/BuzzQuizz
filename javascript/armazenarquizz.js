47df1807c806ddd7edb4911add0c178702dd6605
AleCostaS <alexandrec.silva@usp.br>
2022-07-01 20:34:35 -0300

    Mostrando a tela de sucesso corretamente

--- javascript/armazenarquizz.js
+++ javascript/armazenarquizz.js
@@ -13,12 +13,10 @@ function enviarQuizz (quizz) {
     axios.post('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes', quizz).catch(mostrarErro).then(getId);
 }
 
-let idQuizz;
-let quizzesDoUser = JSON.parse(localStorage.getItem("quizzesUsuario"));
-
-
 function getId (data) { 
+    let idQuizz;
     idQuizz = data.data.id;
+    let quizzesDoUser = JSON.parse(localStorage.getItem("quizzesUsuario"));
     quizzesDoUser.push(idQuizz);
     console.log(quizzesDoUser)
     let arrayStrigified = JSON.stringify(quizzesDoUser);
--- javascript/successcreation.js
+++ javascript/successcreation.js
@@ -1,25 +1,21 @@
-let quizzImg;
-
-function carregarImg(data) {
-    quizzImg = data.data.image;
-    carregarPagina();
-}
-
-function loadpage2 () {
-    const quizatual = axios.get ('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/'+idQuizz);
-    quizatual.catch(mostrarErro).then(carregarPagina);
-}
-
-function carregarPagina(){
+function carregarPagina(data) {
+    console.log(data)
     window.scrollTo(0, 0);
     pagina.innerHTML = 
     `<h1>Seu quizz está pronto!</h1>
     <div class="formatquizzsuccess">
-        <div class="degrade" onclick="exibirQuizz(${idQuizz})" >
-            <img src=${quizzImg} />
+        <div class="degrade" onclick="exibirQuizz(${data.data.id})" >
+            <img src=${data.data.image} />
         </div>
         <p class="titulonaimagem">Titulo do quiz</p>
     </div>
-    <button type="submit" onclick="exibirQuizz(${idQuizz})">Acessar Quizz</button>
+    <button type="submit" onclick="exibirQuizz(${data.data.id})">Acessar Quizz</button>
     <p class="gohome" onclick="criarPagina()">Voltar pra home</p>`;
+}
+
+function loadpage2 () {
+    let quizzesDoUser = JSON.parse(localStorage.getItem("quizzesUsuario"));
+    let idQuizz = quizzesDoUser[quizzesDoUser.length -1];
+    const quizatual = axios.get ('https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes/'+idQuizz);
+    quizatual.catch(mostrarErro).then(carregarPagina);
 }
