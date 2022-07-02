53456b36bb568d9137c569727d13a5a1c85da464
allipski <alicematobalipski@gmail.com>
2022-07-02 14:42:23 -0300

    rolar automaticamente para prox pergunta implementado

--- javascript/logicaquizz.js
+++ javascript/logicaquizz.js
@@ -1,4 +1,5 @@
 function respostaClicada(resposta) {
+    let section
     if (!resposta.parentElement.classList.contains("stop")){
         let kids = resposta.parentElement.children;
         for (let i = 0; i < kids.length; i++) {
@@ -11,6 +12,16 @@ function respostaClicada(resposta) {
         }
         resposta.classList.remove("naoclicada");
         resposta.parentElement.classList.add("stop");
+
+        section = resposta.parentElement.parentElement.parentElement;
     }
-    
+    setTimeout (() => proximaPergunta(section), 2000);
+}
+
+function proximaPergunta(perguntaatual) {
+    console.log(perguntaatual);
+    perguntaatual.nextElementSibling.scrollIntoView({
+        top:80,
+        behavior :'smooth'
+    })
 }
